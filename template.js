efe0083693b03583659ce4761a540c210421b31e
yunus_tekneci <yunus.talha54@hotmail.com>
2025-02-04 00:01:24 +0300

    operators, promt

--- template.js
+++ template.js
@@ -22,39 +22,74 @@ multiple comments
 // // Alert
 // window.alert("Alert Mesajı");
 
-// Variable
-// var, let, const
-// Common Rules
-var myName = "YTT";
-console.log(myName);
+// // Variable
+// // var, let, const
+// // Common Rules
+// var myName = "YTT";
+// console.log(myName);
 
-var myName54 = "YTT";
-console.log(myName54);
+// var myName54 = "YTT";
+// console.log(myName54);
 
-var _myName = "YTT";
-console.log(_myName);
+// var _myName = "YTT";
+// console.log(_myName);
 
-var $_myName = "YTT";
-console.log($_myName);
+// var $_myName = "YTT";
+// console.log($_myName);
 
-// yazılmaz
-// var üğşçö~$_myName = "YTT";
-// console.log(üğşçö~$_myName);
+// // yazılmaz
+// // var üğşçö~$_myName = "YTT";
+// // console.log(üğşçö~$_myName);
 
-let myName2 = "YTT2";
-myName2 = "YTT2 Data";
-console.warn(myName2);
-
-// Kullanamazsın
 // let myName2 = "YTT2";
+// myName2 = "YTT2 Data";
 // console.warn(myName2);
 
-// CONST
-const myName3 = "YTT3 JS";
-console.error(myName3);
+// // Kullanamazsın
+// // let myName2 = "YTT2";
+// // console.warn(myName2);
 
-// const değişmez
-// myName3="YTT JS HTML5";
-// console.error(myName2);
+// // CONST
+// const myName3 = "YTT3 JS";
+// console.error(myName3);
 
+// // const değişmez
+// // myName3="YTT JS HTML5";
+// // console.error(myName2);
+
+// // OPERATORS
+// let yttData1 = 23;
+// let yttData2= 2;
+
+// // Articmatics Operators
+// console.log(yttData1 + yttData2); // 25
+// console.log(yttData1 - yttData2); // 21
+// console.log(yttData1 * yttData2); // 52
+// console.log(yttData1 / yttData2); // 11.5
+
+// // Remainder Operator
+// console.log(yttData1 % yttData2); // 1  
+
+// // Karşılaştırma Operatorleri
+// console.log(yttData1 > yttData2); // true
+// console.log(yttData1 < yttData2); // false
+// console.log(yttData1 >= yttData2); // true
+// console.log(yttData1 <= yttData2); // false
+// console.log(yttData1 == yttData2); // false
+// console.log(yttData1 === yttData2); // false
+
+// // Atama Operatorleri
+// const data1=10, data2="10";
+// console.log(data1 == data2); // true
+// console.log(data1 === data2); // false
+
+// // Mantıksal Operator
+// const data3=true, data4=false;
+// console.log(data3 && data4); // false
+// console.log(data3 || data4); // true
+// console.log(!data3); // false
+
+// // Promt
+// const userData=prompt("Lütfen adınızı giriniz");
+// console.log(userData);
 
