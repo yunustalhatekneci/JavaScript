357818a611667f4ea1dec0b9afbacb5c79250c6a
yunus_tekneci <yunus.talha54@hotmail.com>
2025-02-03 22:47:57 +0300

    console, alert

---
+++ template.js
@@ -0,0 +1,24 @@
+// Single comemnt
+/*
+multiple comments
+*/
+
+// window.
+//document
+console.log("Merhaba");
+
+// Log
+window.console.log("Window Console Log");
+
+// warn
+console.warn("Console Warn"); 
+
+// Info
+console.info("Console Info");
+
+// Error
+console.error("Console Error");
+
+// Alert
+window.alert("Alert Mesajı");
+
