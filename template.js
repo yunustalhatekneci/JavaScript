c05e8c230694931341b2c12c535733f0562baf6b
yunus_tekneci <yunus.talha54@hotmail.com>
2025-02-03 23:20:25 +0300

    variables

--- template.js
+++ template.js
@@ -5,20 +5,56 @@ multiple comments
 
 // window.
 //document
-console.log("Merhaba");
+// console.log("Merhaba");
 
-// Log
-window.console.log("Window Console Log");
+// // Log
+// window.console.log("Window Console Log");
 
-// warn
-console.warn("Console Warn"); 
+// // warn
+// console.warn("Console Warn"); 
 
-// Info
-console.info("Console Info");
+// // Info
+// console.info("Console Info");
 
-// Error
-console.error("Console Error");
+// // Error
+// console.error("Console Error");
 
-// Alert
-window.alert("Alert Mesajı");
+// // Alert
+// window.alert("Alert Mesajı");
+
+// Variable
+// var, let, const
+// Common Rules
+var myName = "YTT";
+console.log(myName);
+
+var myName54 = "YTT";
+console.log(myName54);
+
+var _myName = "YTT";
+console.log(_myName);
+
+var $_myName = "YTT";
+console.log($_myName);
+
+// yazılmaz
+// var üğşçö~$_myName = "YTT";
+// console.log(üğşçö~$_myName);
+
+let myName2 = "YTT2";
+myName2 = "YTT2 Data";
+console.warn(myName2);
+
+// Kullanamazsın
+// let myName2 = "YTT2";
+// console.warn(myName2);
+
+// CONST
+const myName3 = "YTT3 JS";
+console.error(myName3);
+
+// const değişmez
+// myName3="YTT JS HTML5";
+// console.error(myName2);
+
 
